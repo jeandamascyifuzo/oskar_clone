ccdb795c8848952cd6229fa29d4dc5ff37557b3a
Jean_cyifuzo <jeancyifuzodamas@gmail.com>
2025-04-28 22:49:23 +0200

    ft: complete explore

--- src/components/cards/TabCards.tsx
+++ src/components/cards/TabCards.tsx
@@ -1,10 +1,57 @@
 import React from "react";
+import PrimaryBtn from "../buttons/PrimaryBtn";
+import Image from "next/image";
+import Image1 from "@/assets/exploreImages/identity1.svg";
+import Image2 from "@/assets/exploreImages/identity2.svg";
 
-const TabCards: React.FC<{ currentTab: number }> = ({ currentTab }) => {
+interface dataType {
+  currentTab: string;
+  title: string;
+  descrition: string;
+  company: string[];
+  color: string;
+  mainImg: string;
+}
+
+const TabCards: React.FC<dataType> = ({
+  currentTab,
+  title,
+  descrition,
+  company,
+  color,
+  mainImg,
+}) => {
   return (
-    <div>
-      <h1> {`Tabs ${currentTab}`} </h1>
-    </div>
+    <section className="pt-6 px-10">
+      <div className="flex flex-col space-y-4">
+        <h3 className="font-semibold text-xl text-gray-600">{currentTab}</h3>
+        <h1 className="font-medium text-3xl">{title}</h1>
+        <p className="text-base">{descrition}</p>
+        <PrimaryBtn label="Learn more" type="submit" />
+      </div>
+      <hr className="my-4" />
+      <div className="flex items-center justify-between">
+        <p className="text-base">{`Companies building with ${currentTab}`}</p>
+        <div className="flex gap-x-2.5">
+          {company?.map((d, index) => (
+            <Image src={d} width={100} height={10} alt="" key={index} />
+          ))}
+        </div>
+      </div>
+      <div className="mt-4 flex flex-col relative h-60">
+        <div className="flex-1">
+          <div className="mx-auto absolute -bottom-[18rem] left-10">
+            <Image src={mainImg || Image1} width={650} height={10} alt="" />
+          </div>
+        </div>
+        <div
+          className={`h-24  w-full rounded-b-md`}
+          style={{ background: color }}
+        >
+          <Image src={Image2} width={500} height={10} alt="" className="text" />
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/landingPage/ExploreProduct.tsx
+++ src/components/landingPage/ExploreProduct.tsx
@@ -5,6 +5,31 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 import Tabs from "../tab/Tabs";
 import TabCards from "../cards/TabCards";
 import { Fade } from "react-awesome-reveal";
+import dLocal from "@/assets/company/dlocal.svg";
+import food from "@/assets/company/food-court.svg";
+import fair from "@/assets/company/fairmoney.svg";
+import interSwitch from "@/assets/company/interswitch.svg";
+import chaka from "@/assets/company/chaka.svg";
+import glover from "@/assets/company/glover.svg";
+import palm from "@/assets/company/palm-credit.svg";
+import renmoney from "@/assets/company/renmoney.svg";
+import blackcopper from "@/assets/company/blackcopper.svg";
+import swipeng from "@/assets/company/swipeng.svg";
+import aella from "@/assets/company/aella.svg";
+import carbon from "@/assets/company/carbon.svg";
+import credpal from "@/assets/company/credpal.svg";
+import bento from "@/assets/company/bento.svg";
+import autochek from "@/assets/company/autochek.svg";
+import indicina from "@/assets/company/indicina.svg";
+import lendigo from "@/assets/company/lendigo.svg";
+import kippa from "@/assets/company/kippa.svg";
+
+import slider1 from "@/assets/exploreImages/111.svg";
+import slider2 from "@/assets/exploreImages/2222.svg";
+import slider3 from "@/assets/exploreImages/3333.svg";
+import slider4 from "@/assets/exploreImages/4444.svg";
+import slider5 from "@/assets/exploreImages/5555.svg";
+import slider6 from "@/assets/exploreImages/66666.svg";
 
 const ExploreProduct = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -16,31 +41,101 @@ const ExploreProduct = () => {
 
   const tabs = [
     {
-      label: "Profile",
-      content: <TabCards currentTab={activeIndex + 1} />,
+      label: "Identity",
+      content: (
+        <TabCards
+          currentTab={"Identity"}
+          title={"Speed up your onboarding"}
+          descrition={
+            "Reliably sign up your prospects with their KYC profile from any financial institution in seconds."
+          }
+          company={[chaka, glover, palm]}
+          color="#f7d002"
+          mainImg={slider1}
+        />
+      ),
     },
     {
-      label: "Dashboard",
-      content: <TabCards currentTab={activeIndex + 1} />,
+      label: "Income",
+      content: (
+        <TabCards
+          currentTab={"Income"}
+          title={"Supercharge financial management"}
+          descrition={
+            "Design personal financial management experiences with consolidated data."
+          }
+          company={[renmoney, blackcopper, swipeng]}
+          color="#b5f95a"
+          mainImg={slider2}
+        />
+      ),
     },
     {
-      label: "Settings",
-      content: <TabCards currentTab={activeIndex + 1} />,
+      label: "Payments",
+      content: (
+        <TabCards
+          currentTab={"Payments"}
+          title={"Collect bank payments efficiently"}
+          descrition={
+            "Collect one-time and recurring payments from your customers via USSD and internet banking."
+          }
+          company={[interSwitch, dLocal, food]}
+          color="#7664fe"
+          mainImg={slider3}
+        />
+      ),
     },
     {
-      label: "Contact",
-      content: <TabCards currentTab={activeIndex + 1} />,
+      label: "Balance",
+      content: (
+        <TabCards
+          currentTab={"Balance"}
+          title={"Get real-time balance information"}
+          descrition={
+            "Validate the buying power of your customers and reduce failed payments."
+          }
+          company={[fair, aella, carbon]}
+          color="#f7d002"
+          mainImg={slider4}
+        />
+      ),
     },
     {
-      label: "Disabled",
-      content: <TabCards currentTab={activeIndex + 1} />,
+      label: "Transactions",
+      content: (
+        <TabCards
+          currentTab={"Transactions"}
+          title={"Get helpful financial context"}
+          descrition={
+            "Profile customers with transaction history data of up to 24 months."
+          }
+          company={[credpal, bento, autochek]}
+          color="#7664fe"
+          mainImg={slider5}
+        />
+      ),
+    },
+    {
+      label: "Authentication",
+      content: (
+        <TabCards
+          currentTab={"Authentication"}
+          title={"Verify financial credentials"}
+          descrition={
+            "Validate your customers’ financial information in real-time."
+          }
+          company={[indicina, lendigo, kippa]}
+          color="#b5f95a"
+          mainImg={slider6}
+        />
+      ),
     },
   ];
 
   return (
     <section className="flex w-full py-6">
-      <div className="w-[20%] text-gray-600 space-y-4 flex flex-col">
-        <div className="space-y-3">
+      <div className="w-[25%] text-gray-600 space-y-4 flex flex-col">
+        <div className="space-y-3 mb-20">
           <h1 className="font-medium text-3xl">Explore core products</h1>
           <div className="flex items-center space-x-1 group">
             <Link
@@ -59,16 +154,16 @@ const ExploreProduct = () => {
         />
       </div>
       <div className="flex-1 w-full text-gray-600 mr-20">
-        <div className="p-6 text-medium rounded-lg w-full overflow-hidden relative h-full">
+        <div className="text-medium rounded-lg w-full overflow-hidden relative">
           <Fade
             key={activeIndex}
             direction={activeIndex > prevIndex ? "right" : "left"}
-            duration={600}
+            duration={300}
             cascade
             damping={0.1}
             triggerOnce
           >
-            <div className="bg-[#f4f6f8] w-full">
+            <div className="bg-[#f8f4f5] w-full">
               {tabs[activeIndex]?.content}
             </div>
           </Fade>
